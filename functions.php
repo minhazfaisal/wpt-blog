c5cf4aef0e0dfe42f03f193871f14070eaf9f935
Minhaz Faisal <faisalahmed01944@gmail.com>
2026-08-25 05:48:53 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,8 +1,11 @@
 <?php
-
 /*
-* enqueue scripts and styles
+enqueue scripts and styles
 */
 require get_template_directory() . '/inc/enqueue.php';
+/*
+custom menu
+*/
+require get_template_directory() . '/inc/theme_menu.php';
 
 ?>
