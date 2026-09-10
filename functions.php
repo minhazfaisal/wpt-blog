0ffb5aec4e325d1b212a42a9f742639bc30806f4
Minhaz Faisal <faisalahmed01944@gmail.com>
2026-09-11 00:55:52 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -14,4 +14,14 @@ additional theme support
 */
 require get_template_directory() . '/inc/theme_support.php';
 
+// archive.php - archive name or date name display
+add_filter( 'get_the_archive_title', function ( $title ) {
+	if ( is_category() ) {
+		return single_cat_title( '', false );
+	}
+  if ( is_day() ) {
+    return get_the_date( 'F j, Y' );
+  }
+  return $title;
+} );
 ?>
